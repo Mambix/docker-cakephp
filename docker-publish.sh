b5805d5e1e17be5745cac12315e016fcb38adaa2
MambiX Ltd <ledi.mambix@gmail.com>
2020-11-22 00:47:20 +0700

    fix

--- docker-publish.sh
+++ docker-publish.sh
@@ -3,4 +3,4 @@
 gitLabel=`git branch | grep \* | cut -d ' ' -f2`
 docker build --build-arg debug_mode=--no-dev -t mambix/cakephpbase:$gitLabel .
 docker tag mambix/cakephpbase:$gitLabel mambix/cakephpbase:$gitLabel
-docker push mambix/cakephpbase:$gitLabel
+#docker push mambix/cakephpbase:$gitLabel
